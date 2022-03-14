ee479029e8aee201596b451b72f01aa9c9d305e4
madroidmaq <madroidmaq@gmail.com>
2022-03-14 14:01:24 +0800

    ref |> remove ActivityLauncherImpl to file

--- activity-result-launcher/src/main/java/com/madroid/jetpackbox/activity/result/ActivityLauncher.kt
+++ activity-result-launcher/src/main/java/com/madroid/jetpackbox/activity/result/ActivityLauncher.kt
@@ -1,11 +1,8 @@
 package com.madroid.jetpackbox.activity.result
 
 import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.core.app.ActivityOptionsCompat
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlin.coroutines.resume
 
 interface ActivityLauncher<I, O> {
 
@@ -35,36 +32,4 @@ interface ActivityLauncher<I, O> {
      * @throws android.content.ActivityNotFoundException
      */
     suspend fun launch(input: I, options: ActivityOptionsCompat? = null): O
-}
-
-internal class ActivityLauncherImpl<I, O>(
-    private val registry: ActivityResultRegistry,
-    private val contract: ActivityResultContract<I, O>
-) : ActivityLauncher<I, O> {
-
-    private var launcher: androidx.activity.result.ActivityResultLauncher<I>? = null
-    override fun launch(
-        input: I,
-        options: ActivityOptionsCompat?,
-        callback: ActivityResultCallback<O>
-    ) {
-        val key = "com.madroid.jetpackbox#activity-result-launcher#${hashCode()}"
-        registry.register(key, contract) {
-            callback.onActivityResult(it)
-            onResult()
-        }.also {
-            launcher = it
-        }.launch(input, options)
-    }
-
-    private fun onResult() {
-        launcher?.unregister()
-    }
-
-    override suspend fun launch(input: I, options: ActivityOptionsCompat?): O =
-        suspendCancellableCoroutine { continuation ->
-            launch(input, options) { result ->
-                continuation.resume(result)
-            }
-        }
 }
---
+++ activity-result-launcher/src/main/java/com/madroid/jetpackbox/activity/result/ActivityLauncherImpl.kt
@@ -0,0 +1,39 @@
+package com.madroid.jetpackbox.activity.result
+
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.core.app.ActivityOptionsCompat
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
+internal class ActivityLauncherImpl<I, O>(
+    private val registry: ActivityResultRegistry,
+    private val contract: ActivityResultContract<I, O>
+) : ActivityLauncher<I, O> {
+    private var launcher: androidx.activity.result.ActivityResultLauncher<I>? = null
+    override fun launch(
+        input: I,
+        options: ActivityOptionsCompat?,
+        callback: ActivityResultCallback<O>
+    ) {
+        val key = "com.madroid.jetpackbox#activity-result-launcher#${hashCode()}"
+        registry.register(key, contract) {
+            callback.onActivityResult(it)
+            onResult()
+        }.also {
+            launcher = it
+        }.launch(input, options)
+    }
+
+    private fun onResult() {
+        launcher?.unregister()
+    }
+
+    override suspend fun launch(input: I, options: ActivityOptionsCompat?): O =
+        suspendCancellableCoroutine { continuation ->
+            launch(input, options) { result ->
+                continuation.resume(result)
+            }
+        }
+}
